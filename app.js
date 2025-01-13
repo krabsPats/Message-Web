7a62dd1814f80f4020e2bdd0dba8008ab93d90e7
Krab <27kevin.bech@gmail.com>
2025-01-13 14:43:36 +0700

    nah

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let messages = []
     
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname.replace('\\app', '')+'\\frontend\\text.html')
+    res.sendFile(__dirname+'\\index.html')
 })
 
 function POST(){
@@ -71,61 +71,4 @@ app.listen(port,()=>{
 
 
 
-async function docMaker(){
-    const decoder = new TextDecoder
 
-    let data = await fs.readFile(__dirname+'/text.html')    
-    let dataDecode = decoder.decode(data);
-
-
-
-    
-let arr = dataDecode.split("")
-
-let arrIndex = [];
-
-let varAdder = []
-
-for(let i = 0; i < arr.length; i += 1){
-    if(arr[i] == "/" && arr[i+1] == "/" && arr[i+2] == "~"){
-        varAdder.push(i)
-    }
-
-    if(arr[i] == "~" && arr[i+1] == "~"){
-        varAdder.push(i)
-
-    }
-
-    if(varAdder.length == 2){
-        arrIndex.push(varAdder)
-        varAdder = []
-    }
-
-
-}
-
-
-
-
-console.log(arr)    
-console.log(arrIndex)
-
-let content = "";
-
-for(let i = 0; i < arrIndex.length; i += 1){
-    content += (`at index ${ arrIndex[i][0]}`+" - "+arr.slice(arrIndex[i][0]+3,arrIndex[i][1]).join("") +"\n\n")  
-}
-
-console.log(content)
-
-fs.writeFile(__dirname+"\\README.md", content, (err)=>{
-    if(err){
-        console.log("error within writing doc")
-    }
-})
-
-
-}
-
-
-docMaker()
