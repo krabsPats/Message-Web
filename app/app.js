eb6241afde27d9417e75b127a87592aa9828dc52
Krab <27kevin.bech@gmail.com>
2025-01-18 01:07:24 +0700

    Text response adding to the chatbox, inserting info to sql

--- app/app.js
+++ app/app.js
@@ -11,13 +11,13 @@ const { SqlConnection } = require('./sql');
 
 const SSE = [200,{"Content-Type": "text/event-stream", "Cache-Control": "no-cache", Connection: "keep-alive", "access-control-allow-origin": "*",}]
 
-let messages = ["jfje"]
+let messages = [""]
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname+'\\index.html')
+    res.sendFile(__dirname.replace('\\app','\\Front') + '\\index.html')
 })
 
-function POST(){
+
     app.use(bodyParser.text({type: 'text/plain'}))
 
     app.post('/message', (req,res) =>{
@@ -28,41 +28,33 @@ function POST(){
         console.log(messages)
 
         res.send(messages[messages.length-1])
-
-    })
-    
-    
-    
-}
-
-POST()
-
-
-app.get('/messageRetriver', (req, res)=>{
-
-    res.writeHead(SSE[0], SSE[1])
-
-
-
-    res.write(messages[messages.length-1])
-
-    if(messages.length === 3){
-        res.end()
-    }
         
+       
+        const mainDB = new SqlConnection("localhost", 3306, "root", "blue707", "mydb");
+
+        mainDB.Connect((done)=>{
+            console.log(done)
+        })
+
+        mainDB.Query(`insert into message (value) value("${messages[messages.length-1]}");`)
+        
+        mainDB.EndConnection()
+    
+    
+    
 })
+
+
+
 
 app.listen(port,()=>{
     console.log(`server has started at ${port}`)
 })
 
-const mainDB = new SqlConnection("localhost", 3306, "root", "blue707", "mydb");
 
-mainDB.Connect((done)=>{
-    console.log(done)
-})
-mainDB.Query('insert into message (value) value("jgj");')
-mainDB.EndConnection()
+
+
+
 
 
 
